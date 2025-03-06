d8f8d285be71d4f99d79152fe7b94c8fff1f52cf
Script-Savant <alexkienjeku@gmail.com>
2025-03-07 00:33:05 +0300

    Agorithms in Go

---
+++ new/part1/chapter5/Algorithms/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "Algorithms/works"
+)
+
+func main() {
+    list1:= []int {}
+    list2 := [] int {1, 2, 3, 4, 5}
+
+    fmt.Println(works.Sum2(list1))
+    fmt.Println(works.Sum2(list2))
+}
