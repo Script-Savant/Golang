2bfebfbdb070ef1a1b03d8ac57d4a1a796ca6308
Script-Savant <alexkienjeku@gmail.com>
2025-07-11 14:43:52 +0300

    splitting code oin different files

---
+++ booking-app/helper.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+	"fmt"
+	"strings"
+)
+
+// Validates user input
+func validateUserInput(firstName, lastName, email string, userTickets, remainingTickets uint) bool {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@") && len(email) > 5
+	isValidTicketNo := userTickets > 0 && userTickets <= remainingTickets
+
+	if isValidName && isValidEmail && isValidTicketNo {
+		return true
+	}
+
+	// Show error messages
+	if !isValidName {
+		fmt.Println("❌ First or last name entered is too short.")
+	}
+	if !isValidEmail {
+		fmt.Println("❌ Email format entered is incorrect.")
+	}
+	if !isValidTicketNo {
+		fmt.Println("❌ Number of tickets entered is invalid or exceeds availability.")
+	}
+	fmt.Println()
+
+	return false
+}
--- booking-app/main.go
+++ booking-app/main.go
@@ -85,28 +85,3 @@ func printFirstNames(bookings []string) []string {
 	}
 	return firstNames
 }
-
-// Validates user input
-func validateUserInput(firstName, lastName, email string, userTickets, remainingTickets uint) bool {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@") && len(email) > 5
-	isValidTicketNo := userTickets > 0 && userTickets <= remainingTickets
-
-	if isValidName && isValidEmail && isValidTicketNo {
-		return true
-	}
-
-	// Show error messages
-	if !isValidName {
-		fmt.Println("❌ First or last name entered is too short.")
-	}
-	if !isValidEmail {
-		fmt.Println("❌ Email format entered is incorrect.")
-	}
-	if !isValidTicketNo {
-		fmt.Println("❌ Number of tickets entered is invalid or exceeds availability.")
-	}
-	fmt.Println()
-
-	return false
-}
