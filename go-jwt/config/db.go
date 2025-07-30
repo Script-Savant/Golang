ec01cb58951332ad00f8c48e87a442b28db89cf8
Script-Savant <alexkienjeku@gmail.com>
2025-07-30 16:33:07 +0300

    update db conf to parse time

--- go-jwt/config/db.go
+++ go-jwt/config/db.go
@@ -15,7 +15,7 @@ func ConnectDatabase() {
 	open the connection using gorm
 	store the connection to the DB variable
 	*/
-	dsn := "root:password@tcp(127.0.0.1:3306)/go_jwt_auth"
+	dsn := "root:password@tcp(127.0.0.1:3306)/go_jwt_auth?parseTime=True"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
