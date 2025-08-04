2995c4a1b04a271b23f20defc620569b07a12e8d
Script-Savant <alexkienjeku@gmail.com>
2025-08-04 22:07:44 +0300

    conf db, router and database migration

--- go-blog/main.go
+++ go-blog/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "go-blog/config"
+import (
+	"go-blog/config"
+	"go-blog/models"
+	"go-blog/routes"
+	"log"
+)
 
 func main() {
 	// connect database
 	config.ConnectDatabase()
+
+	// auto migrate models
+	db := config.DB
+	err := db.AutoMigrate(
+		&models.User{},
+		&models.Profile{},
+		&models.Post{},
+		&models.Tag{},
+		&models.Comment{},
+		&models.Like{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	// setup the router with all routes
+	router := routes.SetupRoutes(db)
+
+	// start the server
+	log.Println("Server starting on :8080...")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
