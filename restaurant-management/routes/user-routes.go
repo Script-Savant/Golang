eea432d7ae11f5fce57eae21ed705b055eae043c
Script-Savant <alexkienjeku@gmail.com>
2025-08-07 02:40:50 +0300

    user and update user routes

--- restaurant-management/routes/user-routes.go
+++ restaurant-management/routes/user-routes.go
@@ -4,16 +4,21 @@ package routes
 
 import (
 	"golang-restaurant-management/controllers"
+	"golang-restaurant-management/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterUserRoutes - add register and login endpoints to the the router
-// public routes
 func RegisterUserRoutes(r *gin.Engine) {
 	users := r.Group("api/users/")
 	{
+		// public routes
 		users.POST("register", controllers.Register)
 		users.POST("login", controllers.Login)
+
+		// protected routes
+		users.GET("me", middleware.JWTAuthMiddleware(), controllers.Me)
+		users.PUT("me", middleware.JWTAuthMiddleware(), controllers.UpdateMe)
 	}
 }
