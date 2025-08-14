4ca24695610e4936d4f8740598445f99aa02a01c
Script-Savant <alexkienjeku@gmail.com>
2025-08-15 01:43:48 +0300

    how to create a block

---
+++ go-blockchain/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.nonce = nonce
+	b.previousHash = previousHash
+	b.timestamp = time.Now().UnixNano()
+
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp      %d\n", b.timestamp)
+	fmt.Printf("nonce          %d\n", b.nonce)
+	fmt.Printf("previous_hash  %s\n", b.previousHash)
+	fmt.Printf("transactions   %s\n", b.transactions)
+}
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	b := NewBlock(0, "prev hash")
+	b.Print()
+}
