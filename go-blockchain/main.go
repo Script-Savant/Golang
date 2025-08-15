dee8e24b57ce77011deba1913a3356ef1188d1d9
Script-Savant <alexkienjeku@gmail.com>
2025-08-15 03:18:34 +0300

    create a blockchain struct

--- go-blockchain/main.go
+++ go-blockchain/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
+// block struct - nonce, timestamp, transactions, previous hash
 type Block struct {
 	nonce        int
 	previousHash string
@@ -13,6 +15,7 @@ type Block struct {
 	transactions []string
 }
 
+// Create a new block and return it
 func NewBlock(nonce int, previousHash string) *Block {
 	b := new(Block)
 	b.nonce = nonce
@@ -29,11 +32,43 @@ func (b *Block) Print() {
 	fmt.Printf("transactions   %s\n", b.transactions)
 }
 
+// blockchain structure - pool of transactions, chain of blocks
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+// create a new block chain and return it
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "init hash")
+	return bc
+}
+
+// create block and append it to block chain
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n\n", strings.Repeat("*", 25))
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	b := NewBlock(0, "prev hash")
-	b.Print()
+	bc := NewBlockchain()
+	bc.Print()
+	bc.CreateBlock(5, "hash 1")
+	bc.Print()
+	bc.CreateBlock(10, "hash 2")
+	bc.Print()
 }
