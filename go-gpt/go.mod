b8b9ec272dd70c3814e3c0a6aba4253b5a1448dc
Script-Savant <alexkienjeku@gmail.com>
2025-09-08 13:07:57 +0300

    chatgpt with go

---
+++ go-gpt/go.mod
@@ -0,0 +1,8 @@
+module go-gpt
+
+go 1.25
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sashabaranov/go-openai v1.41.1
+)
--- go-gpt/main.go
+++ go-gpt/main.go
@@ -1,1 +1,41 @@
-package go_gpt
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env file not found")
+	}
+
+	apiKey := os.Getenv("API_KEY")
+	ctx := context.Background()
+	client := openai.NewClient(apiKey)
+
+	resp, err := client.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "How many cups of coffee should i have?",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
