07be3ab395a374e2fc05dafbea5e26534d7d4d7a
Script-Savant <alexkienjeku@gmail.com>
2025-09-09 13:21:55 +0300

    pour me some gin

---
+++ go-gin/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Message struct {
+	Name  string `json:"name"`
+	Email string `json:"email" binding:"requiured,email"`
+}
+
+func router() *gin.Engine {
+	r := gin.Default()
+	userRoute := r.Group("/user")
+	{
+		userRoute.GET("/hello/:name", func(c *gin.Context) {
+			user := c.Param("name")
+			response := fmt.Sprintf("hello %s", user)
+			c.String(http.StatusOK, response)
+		})
+
+		userRoute.POST("/post", func(c *gin.Context) {
+			body := Message{}
+			if err := c.ShouldBindJSON(&body); err != nil {
+				c.AbortWithError(http.StatusBadRequest, err)
+				return
+			}
+			fmt.Println(body)
+			c.JSON(http.StatusAccepted, &body)
+		})
+
+		userRoute.POST("/upload", func(c *gin.Context) {
+			file, _ := c.FormFile("file")
+			log.Println(file.Filename)
+
+			c.SaveUploadedFile(file, "/tmp/tempfile")
+
+			c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+		})
+	}
+	return r
+}
+
+func main() {
+	r := router()
+	r.Run()
+}
