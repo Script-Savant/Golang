9508d1736ac5a1bf6e06419bb336d4742f548303
Script-Savant <alexkienjeku@gmail.com>
2025-09-10 17:50:23 +0300

    later

--- dapp-tutorial/main.go
+++ dapp-tutorial/main.go
@@ -2,97 +2,44 @@ package main
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"log"
-	"math/big"
-	"net/http"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/gin-gonic/gin"
 )
 
-var client *ethclient.Client
-
 func main() {
-	var err error
 	client, err := ethclient.Dial("http://127.0.0.1:8545")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer client.Close()
 
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
+	block, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	r.Static("/static", "./static") // for htmx and bootstrap
+	fmt.Println("Connected to Geth - latest block", block.Number().Uint64())
+}
 
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
-	})
+func GenerateKeyPair() (privateKey *ecdsa.PrivateKey, address string, err error) {
+	priv, err := crypto.GenerateKey()
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to generate key: %w", err)
+	}
 
-	r.GET("/api/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+	privBytes := crypto.FromECDSA(priv)
+	fmt.Printf("PRIVATE KEY (dev only) 0x%x\n", privBytes)
 
-	r.POST("/api/register", func(c *gin.Context) {
-		username := c.PostForm("username")
-		email := c.PostForm("email")
+	pub := priv.Public()
+	pubECDSA := pub.(*ecdsa.PublicKey)
+	addr := crypto.PubkeyToAddress(*pubECDSA)
+	addrHex := addr.Hex()
 
-		c.String(200, "Registered user %s with email %s", username, email)
-	})
+	fmt.Println("Address: ", addrHex)
 
-	r.GET("/block-number", func(c *gin.Context) {
-		block, err := client.BlockByNumber(context.Background(), nil)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error %s", err.Error())
-			return
-		}
-		c.String(http.StatusOK, fmt.Sprintf("Latest block: %d", block.Number().Uint64()))
-	})
-
-	r.GET("/latest-blocks", func(c *gin.Context) {
-		head, err := client.BlockByNumber(context.Background(), nil)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error: %s", err.Error())
-			return
-		}
-
-		html := "<ul class='list-group'>"
-		for i := 0; i < 5; i++ {
-			num := head.Number().Uint64() - uint64(i)
-			blk, _ := client.BlockByNumber(context.Background(), big.NewInt(int64(num)))
-			html += fmt.Sprintf("<li class='list-group-item'>Block %d -> %s</li>", blk.Number().Uint64(), blk.Hash().Hex())
-		}
-		html += "</ul>"
-
-		c.Header("Content-Type", "text/html")
-		c.String(http.StatusOK, html)
-	})
-
-	r.POST("/balance", func(c *gin.Context) {
-		address := c.PostForm("address")
-		acc := common.HexToAddress(address)
-
-		balance, err := client.BalanceAt(context.Background(), acc, nil)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error: %s", err.Error())
-			return
-		}
-
-		c.String(http.StatusOK, fmt.Sprintf("Balance: %s Wei", balance.String()))
-	})
-
-	r.GET("/watch-balance/:address", func(c *gin.Context) {
-		addr := c.Param("address")
-		acc := common.HexToAddress(addr)
-
-		balance, err := client.BalanceAt(context.Background(), acc, nil)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error: %s", err.Error())
-			return
-		}
-		c.String(http.StatusOK, fmt.Sprintf("Balance: %s", balance.String()))
-	})
-
-	r.Run(":8080")
+	return priv, addrHex, nil
 }
