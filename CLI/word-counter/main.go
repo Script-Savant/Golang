8f2e0e8f63d28481dcce60d6cc9dc410922d229a
Script-Savant <alexkienjeku@gmail.com>
2025-10-02 14:36:22 +0300

    count bytes

--- CLI/word-counter/main.go
+++ CLI/word-counter/main.go
@@ -11,15 +11,21 @@ import (
 func main() {
 
 	lines := flag.Bool("l", false, "Count lines")
+	baits := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *baits))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	switch {
+	case countLines:
+		scanner.Split(bufio.ScanLines)
+	case countBytes:
+		scanner.Split(bufio.ScanBytes)
+	default:
 		scanner.Split(bufio.ScanWords)
 	}
 
